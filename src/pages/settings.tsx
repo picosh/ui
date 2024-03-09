160191fe10b6281de79a5f77f1c184a7fbd0098a
Eric Bower <me@erock.io>
2024-03-09 11:40:46 -0500

    tables

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,7 +1,18 @@
+import { fetchFeatures, fetchPubkeys, fetchTokens } from "@app/api";
+import { FeaturesTable, PubkeysTable, TokensTable, UserBox } from "@app/shared";
+import { useQuery } from "starfx/react";
+
 export function SettingsPage() {
+  useQuery(fetchTokens());
+  useQuery(fetchPubkeys());
+  useQuery(fetchFeatures());
+
   return (
-    <div className="flex justify-center mt-4">
-      <img src="/under-construction.gif" alt="under construction" />
+    <div className="group">
+      <UserBox />
+      <PubkeysTable />
+      <TokensTable />
+      <FeaturesTable />
     </div>
   );
 }
--- src/shared.tsx
+++ src/shared.tsx
@@ -1,6 +1,11 @@
+import { plusUrl } from "@app/router";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { useApi, useLoaderSuccess, useQuery } from "starfx/react";
 import { fetchOrCreateToken, registerUser, schema, useSelector } from "./api";
+
+const bytesToMb = (num: number) => num / 1000 / 1000;
+const bytesToGb = (num: number) => bytesToMb(num) / 1000;
 
 export function Banner({ children }: { children: React.ReactNode }) {
   return <div>{children}</div>;
@@ -61,7 +66,7 @@ export const RssBox = () => {
         and highly recommended.
       </p>
 
-      <pre>https://auth.pico.sh/rss/{token.id}</pre>
+      <pre>https://auth.pico.sh/rss/{token}</pre>
 
       <h4 className="text-md">
         <a href="https://pico.sh/feeds">feeds.sh</a>
@@ -72,7 +77,7 @@ export const RssBox = () => {
 
       <pre>{`=: email rss@myemail.com
 =: digest_interval 1day
-=> https://auth.pico.sh/rss/${token.id}`}</pre>
+=> https://auth.pico.sh/rss/${token}`}</pre>
 
       <pre>rsync pico.txt feeds.sh:/</pre>
     </div>
@@ -123,5 +128,148 @@ export function SignupForm({ onSuccess }: { onSuccess: () => void }) {
         <a href="https://pico.sh/privacy">privacy policy</a>.
       </p>
     </form>
+  );
+}
+
+export function FeaturesTable() {
+  const features = useSelector(schema.features.selectTableAsList);
+  if (features.length === 0) {
+    return (
+      <div className="group box">
+        <div>
+          You are not a <code>pico+</code> member.
+        </div>
+        <div>
+          <Link to={plusUrl()} className="btn-link">
+            JOIN
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <table className="w-full box">
+      <thead>
+        <tr>
+          <th className="text-left">Feature</th>
+          <th className="text-left">Expiration</th>
+          <th className="text-center">Storage Max (GB)</th>
+          <th className="text-center">File Max (MB)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {features.length === 0 ? (
+          <tr>
+            <td colSpan={4} className="text-center">
+              No features!
+            </td>
+          </tr>
+        ) : null}
+        {features.map((feature) => {
+          return (
+            <tr key={feature.id}>
+              <td className="text-left">{feature.name}</td>
+              <td className="text-left">
+                {new Date(feature.expires_at).toDateString()}
+              </td>
+              <td className="text-center">
+                {bytesToGb(feature.data.storage_max)}
+              </td>
+              <td className="text-center">
+                {bytesToMb(feature.data.file_max)}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export function PubkeysTable() {
+  const user = useSelector(schema.user.select);
+  const pubkeys = useSelector(schema.pubkeys.selectTableAsList);
+  return (
+    <table className="w-full box">
+      <thead>
+        <tr>
+          <th className="text-left">Key</th>
+          <th className="text-left">Created At</th>
+        </tr>
+      </thead>
+      <tbody>
+        {pubkeys.length === 0 ? (
+          <tr>
+            <td colSpan={4} className="text-center">
+              No pubkeys?
+            </td>
+          </tr>
+        ) : null}
+        {pubkeys.map((pubkey) => {
+          return (
+            <tr key={pubkey.id}>
+              <td className="text-left">
+                {pubkey.key === user.pubkey ? "*" : ""} {pubkey.key}
+              </td>
+              <td className="text-left">
+                {new Date(pubkey.created_at).toDateString()}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export function TokensTable() {
+  const tokens = useSelector(schema.tokens.selectTableAsList);
+  return (
+    <table className="w-full box">
+      <thead>
+        <tr>
+          <th className="text-left">Name</th>
+          <th className="text-left">Created At</th>
+          <th className="text-left">Expires At</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tokens.length === 0 ? (
+          <tr>
+            <td colSpan={4} className="text-center">
+              No tokens found
+            </td>
+          </tr>
+        ) : null}
+        {tokens.map((token) => {
+          return (
+            <tr key={token.id}>
+              <td className="text-left">{token.name}</td>
+              <td className="text-left">
+                {new Date(token.created_at).toDateString()}
+              </td>
+              <td className="text-left">
+                {new Date(token.expires_at).toDateString()}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export function UserBox() {
+  const user = useSelector(schema.user.select);
+  return (
+    <div className="box">
+      <div>
+        <span className="font-bold">pubkey:</span> {user.pubkey}
+      </div>
+      <div>
+        <span className="font-bold">username:</span> {user.username}
+      </div>
+    </div>
   );
 }
